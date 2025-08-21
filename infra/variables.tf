ff6e62e4d02aa6968f2ec854af034acfce20b822
tarik.baki <tarik.baki@mobilexpress.com.tr>
2025-08-21 14:57:13 +0300

    add acm_certificate_arn

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,3 +3,7 @@ variable "aws_region"  { type = string, default = "us-east-1" }
 variable "vpc_cidr"    { type = string, default = "10.0.0.0/16" }
 variable "instance_type" { type = string, default = "t3.micro" }
 variable "ami_id"      { type = string, default = "ami-08c40ec9ead489470" } # Amazon Linux 2
+variable "acm_certificate_arn" {
+  description = "ACM certificate ARN used by ALB HTTPS (443)"
+  type        = string
+}
